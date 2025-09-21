9247498676cfe266f0199f87bf7eea711c4b2e40
D1ko4 <diasmaratov36@gmail.com>
2025-09-22 04:48:28 +0500

    statis style for welcome page

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template('index.html')
 
 @app.route("/healthz")
 def healthz():
-    return "ok", 200
+    return render_template('health.html'), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
