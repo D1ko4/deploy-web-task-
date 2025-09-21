794ac5a18a2f0910a5e0efd39896305d045fdb60
D1ko4 <diasmaratov36@gmail.com>
2025-09-22 04:16:20 +0500

    fix with addign ports

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route("/healthz")
+def healthz():
+    return "ok", 200
+
 if __name__ == "__main__":
     app.run(debug=True)
     
